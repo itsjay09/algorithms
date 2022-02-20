3b5f9bfce3521e856d213ebacbce06259df13ad4
jayeshpatil <jayesh.patil1594@gmail.com>
2022-02-20 09:45:34 +0530

    Merge Sort with optimize logic

--- src/algorithms/learning/sort/MergeSort.java
+++ src/algorithms/learning/sort/MergeSort.java
@@ -5,20 +5,22 @@ import java.util.Arrays;
 public class MergeSort {
 
     public static void main(String[] args) {
-
         int[] array = {12, 11, 13, 5, 6, 7};
-        System.out.println("Given : { " +Arrays.toString(array) +" }");
-        mergeSort(array, 0, array.length -1);
-        System.out.println("Sorted : { " +Arrays.toString(array) +" }");
+        System.out.println("Given : { " + Arrays.toString(array) + " }");
+        mergeSort(array, 0, array.length - 1);
+        System.out.println("Sorted : { " + Arrays.toString(array) + " }");
     }
 
     public static void mergeSort(int[] array, int startIndex, int endIndex) {
-        if (startIndex < endIndex) {
-            int midIndex = (endIndex + startIndex )/ 2;
-            mergeSort(array, startIndex, midIndex);
-            mergeSort(array, midIndex + 1, endIndex);
-            merge(array, startIndex, midIndex, endIndex);
+
+        if ((endIndex - startIndex) < 2) {
+            return;
         }
+
+        int midIndex = (endIndex + startIndex) / 2;
+        mergeSort(array, startIndex, midIndex);
+        mergeSort(array, midIndex + 1, endIndex);
+        merge(array, startIndex, midIndex, endIndex);
     }
 
     private static void merge(int[] array, int startIndex, int midIndex, int endIndex) {
@@ -33,34 +35,26 @@ public class MergeSort {
         // compare both array and merge
         while (i <= midIndex && j <= endIndex) {
             if (array[i] < array[j]) {
-                tempArray[k] = array[i];
-                i++;
+                tempArray[k++] = array[i++];
             } else {
-                tempArray[k] = array[j];
-                j++;
+                tempArray[k++] = array[j++];
             }
-            k++;
         }
 
         // marge remaining elements
         if (i > midIndex) {
             while (j <= endIndex) {
-                tempArray[k] = array[j];
-                k++;
-                j++;
+                tempArray[k++] = array[j++];
             }
         } else {
             while (i <= midIndex) {
-                tempArray[k] = array[i];
-                k++;
-                i++;
+                tempArray[k++] = array[i++];
             }
         }
 
         //copy temp in to main array
         for (int l = 0; l < tempArray.length; l++) {
-            array[startIndex] = tempArray[l];
-            startIndex++;
+            array[startIndex++] = tempArray[l];
         }
     }
 }
