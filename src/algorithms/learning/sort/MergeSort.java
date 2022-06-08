f36e3e0c9ca99abe0b531a368104c8a87aa8eb00
jayeshpatil <jayesh.patil1594@gmail.com>
2022-06-08 21:43:06 +0530

    Practice Merge Sort

--- src/algorithms/learning/sort/MergeSort.java
+++ src/algorithms/learning/sort/MergeSort.java
@@ -1,60 +1,115 @@
 package algorithms.learning.sort;
 
 import java.util.Arrays;
+import java.util.Random;
 
 public class MergeSort {
 
     public static void main(String[] args) {
+
+        Random random = new Random();
+        int[] numbers = new int[10];
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = random.nextInt(100);
+        }
+
         int[] array = {12, 11, 13, 5, 6, 7};
-        System.out.println("Given : { " + Arrays.toString(array) + " }");
-        mergeSort(array, 0, array.length - 1);
-        System.out.println("Sorted : { " + Arrays.toString(array) + " }");
+        System.out.println("Given : { " + Arrays.toString(numbers) + " }");
+        mergeSort(numbers);
+        System.out.println("Sort : { " + Arrays.toString(numbers) + " }");
     }
 
-    public static void mergeSort(int[] array, int startIndex, int endIndex) {
-
-        if ((endIndex - startIndex) < 2) {
+    private static void mergeSort(int[] array) {
+        int length = array.length;
+        if (length < 2) {
             return;
         }
 
-        int midIndex = (endIndex + startIndex) / 2;
-        mergeSort(array, startIndex, midIndex);
-        mergeSort(array, midIndex + 1, endIndex);
-        merge(array, startIndex, midIndex, endIndex);
+        int mid = length / 2;
+        int[] leftArray = new int[mid];
+        int[] rightArray = new int[length - mid];
+
+        for (int i = 0; i < mid; i++) {
+            leftArray[i] = array[i];
+        }
+
+        for (int i = mid; i < length; i++) {
+            rightArray[i - mid] = array[i];
+        }
+
+        mergeSort(leftArray);
+        mergeSort(rightArray);
+        mergeArray(array, leftArray, rightArray);
     }
 
-    private static void merge(int[] array, int startIndex, int midIndex, int endIndex) {
+    private static void mergeArray(int[] array, int[] leftArray, int[] rightArray) {
+        int leftLength = leftArray.length;
+        int rightLength = rightArray.length;
 
-        int i = startIndex;
-        int j = midIndex + 1;
-        int k = 0;
+        int i = 0, j = 0, k = 0;
 
-        //create temp array
-        int[] tempArray = new int[endIndex - startIndex + 1];
-
-        // compare both array and merge
-        while (i <= midIndex && j <= endIndex) {
-            if (array[i] < array[j]) {
-                tempArray[k++] = array[i++];
-            } else {
-                tempArray[k++] = array[j++];
+        while (i < leftLength && j < rightLength) {
+            if(leftArray[i] <= rightArray[j]){
+                array[k++] = leftArray[i++];
+            }else{
+                array[k++] = rightArray[j++];
             }
         }
 
-        // marge remaining elements
-        if (i > midIndex) {
-            while (j <= endIndex) {
-                tempArray[k++] = array[j++];
-            }
-        } else {
-            while (i <= midIndex) {
-                tempArray[k++] = array[i++];
-            }
+        while( i < leftLength){
+            array[k++] = leftArray[i++];
         }
 
-        //copy temp in to main array
-        for (int l = 0; l < tempArray.length; l++) {
-            array[startIndex++] = tempArray[l];
+        while (j < rightLength){
+            array[k++]= rightArray[j++];
         }
     }
+
+
+//    public static void mergeSort(int[] array, int startIndex, int endIndex) {
+//
+//        if ((endIndex - startIndex) < 2) {
+//            return;
+//        }
+//
+//        int midIndex = (endIndex + startIndex) / 2;
+//        mergeSort(array, startIndex, midIndex);
+//        mergeSort(array, midIndex + 1, endIndex);
+//        merge(array, startIndex, midIndex, endIndex);
+//    }
+//
+//    private static void merge(int[] array, int startIndex, int midIndex, int endIndex) {
+//
+//        int i = startIndex;
+//        int j = midIndex + 1;
+//        int k = 0;
+//
+//        //create temp array
+//        int[] tempArray = new int[endIndex - startIndex + 1];
+//
+//        // compare both array and merge
+//        while (i <= midIndex && j <= endIndex) {
+//            if (array[i] < array[j]) {
+//                tempArray[k++] = array[i++];
+//            } else {
+//                tempArray[k++] = array[j++];
+//            }
+//        }
+//
+//        // marge remaining elements
+//        if (i > midIndex) {
+//            while (j <= endIndex) {
+//                tempArray[k++] = array[j++];
+//            }
+//        } else {
+//            while (i <= midIndex) {
+//                tempArray[k++] = array[i++];
+//            }
+//        }
+//
+//        //copy temp in to main array
+//        for (int l = 0; l < tempArray.length; l++) {
+//            array[startIndex++] = tempArray[l];
+//        }
+//    }
 }
